d5dc671ca549a011e1a541f49e403b67de28918b
Arthur Silva <fuscellaarthur@gmail.com>
2020-04-04 16:57:04 +0000

    Application initial version

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.br.ButtonFactory.controller.Main", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf com.br.ButtonFactory.view.Main
+		 */
+		onInit: function () {
+
+		}
+
+	});
+
+});
